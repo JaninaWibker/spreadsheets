c0590f330f143c4a56cc2dd5ddc50e53c38a81c8
Jannik Wibker <jannikwibker@gmail.com>
2020-08-26 21:28:03 +0200

    cleaned up the code a bit and added TODOs identifying problems / possible features (the elephant in the room that the whole state management needs a make over is not explicitely mentioned)

--- src/components/Spreadsheet.js
+++ src/components/Spreadsheet.js
@@ -32,9 +32,6 @@ export default class Spreadsheet extends Component {
     this.columns = this.data[0].length
     this.rows    = this.data.length
 
-    this.update  = this.update.bind(this)
-    this._update = this._update.bind(this)
-
     this.props.cb(this.data, this.update)
 
     this.state = {
@@ -58,6 +55,8 @@ export default class Spreadsheet extends Component {
       }
     }
 
+    // TODO: improve the way cells are labelled / identified
+
     // g stands for get and was used directly before a proper excel syntax parser was implemented, that is why the name is so short
     const g = (cell, call_cell, byRender=false, rec_call_cell) => {
       //console.log(cell, call_cell, byRender, rec_call_cell)
@@ -66,7 +65,7 @@ export default class Spreadsheet extends Component {
       if(cell.indexOf('.') === -1) {
         const match = cell.match(/^([a-zA-Z])+([0-9]+)$/)
         if(match) {
-          // possible bug: would this fail for 'ABC213'?
+          // possible bug: would this fail for 'ABC213' because only one letter is considered?
           s = [parseInt(match[2], 10)-1, Alphabet.indexOf(match[1].toUpperCase())] // excel-like format (A1, B5, ...)
         } else {
           const identifierMatch = cell.match(/^[a-zA-Z_][a-zA-Z_0-9]*$/)
@@ -146,24 +145,24 @@ export default class Spreadsheet extends Component {
     }
   }
 
-  _update(cell) {
-    const s = cell.split('.')
-    const c = this.data[s[0]][s[1]]
+  _update = (cell_id) => {
+    const [col, row] = cell_id.split('.')
+    const cell = this.data[col][row]
 
-    this.data[s[0]][s[1]].visited = false
+    this.data[col][row].visited = false
 
-    if(c.changes) c.changes.map(this.update)
+    if(cell.changes) cell.changes.map(this.update)
   }
 
-  update(cell) {
-    this._update(cell)
+  update = (cell_id) => {
+    this._update(cell_id)
     this.forceUpdate()
   }
 
   render_cell(cell) {
-    const s = cell.id.split('.')
+    const [col, row] = cell.id.split('.')
     if(cell.name) {
-      IDENTIFIER_CELLS[cell.name] = [s[0], s[1]]
+      IDENTIFIER_CELLS[cell.name] = [col, row]
     }
     const editable = (cell.tp === 'NUMBER' || cell.tp === 'STRING')
 
@@ -173,17 +172,25 @@ export default class Spreadsheet extends Component {
     const cb = (value) => {
       // if formula then assign new value to .vl and compute .fn
       if(value.startsWith('=')) {
-        this.data[s[0]][s[1]].vl = value
-        this.data[s[0]][s[1]].fn = parse_formula(value.substring(1))
+        this.data[col][row].vl = value
+        this.data[col][row].fn = parse_formula(value.substring(1))
       } else {
-        if(this.data[s[0]][s[1]].tp === 'NUMBER') {
+        if(this.data[col][row].tp === 'NUMBER') {
           if(isNaN(parseFloat(value))) {
-            // this.data[s[0]][s[1]].vl // TODO: what is this even used for?
+            console.log(this.data[col][row])
+            // TODO: what should happen here? this is what gets run if the value that is inputted is not a
+            // TODO: number, but should the error be generated here or somewhere else; considering that no
+            // TODO: other input is ever somehow flagged as invalid (you can perfectly well place strings
+            // TODO: in a NUMBER cell using a reference) should this even be an error? I feel like there
+            // TODO: should be stronger safe-guards against this and errors should be displayed using the
+            // TODO: same mechanism rendered markdown is displayed. This means that everything tagged as a
+            // TODO: number should display an error if the content of the cell is not interpretable as a number.
+            // this.data[row][col].vl = "#NaN"
           } else {
-            this.data[s[0]][s[1]].vl = parseFloat(value)
+            this.data[col][row].vl = parseFloat(value)
           }
-        } else if(this.data[s[0]][s[1]].tp === 'STRING') {
-          this.data[s[0]][s[1]].vl = value
+        } else if(this.data[col][row].tp === 'STRING') {
+          this.data[col][row].vl = value
         }
       }
 
@@ -192,56 +199,71 @@ export default class Spreadsheet extends Component {
 
     const v = this.g(cell.id, cell.id, true, cell.id)
 
+    const sel_cb = (e, id) => {
+      const [col, row] = id.split('.')
+      if(e.type === 'mousedown')
+        this.setState({selection: {
+          start_x: parseInt(row, 10),
+          start_y: parseInt(col, 10),
+          end_x: parseInt(row, 10),
+          end_y: parseInt(col, 10)
+        }})
+      else if(e.type === 'mouseup')
+        this.setState({selection: {
+          start_x: this.state.selection.start_x,
+          start_y: this.state.selection.start_y,
+          end_x: parseInt(row, 10),
+          end_y: parseInt(col, 10)
+        }})
+      else if(e.type === 'mouseenter' && e.buttons === 1)
+        this.setState({selection: {
+          start_x: this.state.selection.start_x,
+          start_y: this.state.selection.start_y,
+          end_x: parseInt(row, 10),
+          end_y: parseInt(col, 10)
+        }})
+    }
+
+    const handle_arrow_keys = (key, shift, alt, ctrl, preventDefault) => {
+      const sx = { ArrowLeft: -1, ArrowRight: 1 }[key] || 0
+      const sy = { ArrowUp:   -1, ArrowDown:  1 }[key] || 0
+
+      // TODO: ctrl snaps to edge (should also work together with alt)
+
+      // TODO: tab should select the next cell (shift tab inverse; ctrl either no difference or input completely ignored)
+
+      // TODO: if currently at pos1 and using shift to select an area that spans
+      // TODO: from pos1 to pos2 and then pressing an arrow key without shift or
+      // TODO: alt it should act as if the current position is pos2, not pos1
+      // TODO: which is how it currently works
+
+      // TODO: set proper focus to the cell underneath the cursor; this allows pressing enter or similar to start editing
+
+      preventDefault()
+
+      this.setState({selection: {
+        start_x: Math.min(this.state.dimensions.x-1, Math.max(0, this.state.selection.start_x + ((shift && !alt) ? 0 : sx))),
+        start_y: Math.min(this.state.dimensions.y-1, Math.max(0, this.state.selection.start_y + ((shift && !alt) ? 0 : sy))),
+        end_x: Math.min(this.state.dimensions.x-1, Math.max(0, ((alt || shift) ? this.state.selection.end_x : this.state.selection.start_x) + sx)),
+        end_y: Math.min(this.state.dimensions.y-1, Math.max(0, ((alt || shift) ? this.state.selection.end_y : this.state.selection.start_y) + sy)),
+      }, focused: {
+        x: this.state.selection.start_x,
+        y: this.state.selection.start_y
+      }}, _ => {
+        if(this.selectionElement) scrollIntoViewIfNeeded(this.selectionElement)
+      })
+    }
+
     return (
       <Cell
-      key={s[0] + '.' + s[1]}
-      id={s[0] + '.' + s[1]}
+      key={col + '.' + row}
+      id={col + '.' + row}
       content={format_data(v, cell.tp, cell.stp, cell.r_dec || this.props.options.rounding)}
       editable={editable}
       style={cell.style ? cell.style : null}
       cb={cb}
-      sel_cb={(e, id) => {
-        const s = id.split('.')
-        if(e.type === 'mousedown')
-          this.setState({selection: {
-            start_x: parseInt(s[1], 10),
-            start_y: parseInt(s[0], 10),
-            end_x: parseInt(s[1], 10),
-            end_y: parseInt(s[0], 10)
-          }})
-        else if(e.type === 'mouseup')
-          this.setState({selection: {
-            start_x: this.state.selection.start_x,
-            start_y: this.state.selection.start_y,
-            end_x: parseInt(s[1], 10),
-            end_y: parseInt(s[0], 10)
-          }})
-        else if(e.type === 'mouseenter' && e.buttons === 1)
-          this.setState({selection: {
-            start_x: this.state.selection.start_x,
-            start_y: this.state.selection.start_y,
-            end_x: parseInt(s[1], 10),
-            end_y: parseInt(s[0], 10)
-          }})
-      }}
-      handleArrowKeys={(key, shift, alt, ctrl, preventDefault) => {
-        const sx = { ArrowLeft: -1, ArrowRight: 1 }[key] || 0
-        const sy = { ArrowUp:   -1, ArrowDown:  1 }[key] || 0
-
-        preventDefault()
-
-        this.setState({selection: {
-          start_x: Math.min(this.state.dimensions.x-1, Math.max(0, this.state.selection.start_x + ((shift && !alt) ? 0 : sx))),
-          start_y: Math.min(this.state.dimensions.y-1, Math.max(0, this.state.selection.start_y + ((shift && !alt) ? 0 : sy))),
-          end_x: Math.min(this.state.dimensions.x-1, Math.max(0, ((alt || shift) ? this.state.selection.end_x : this.state.selection.start_x) + sx)),
-          end_y: Math.min(this.state.dimensions.y-1, Math.max(0, ((alt || shift) ? this.state.selection.end_y : this.state.selection.start_y) + sy)),
-        }, focused: {
-          x: this.state.selection.start_x,
-          y: this.state.selection.start_y
-        }}, x => {
-          if(this.selectionElement) scrollIntoViewIfNeeded(this.selectionElement)
-        })
-      }}
+      sel_cb={sel_cb}
+      handleArrowKeys={handle_arrow_keys}
       raw_data={cell.fn ? cell.vl : v}
       tp={cell.tp} />
     )
@@ -261,13 +283,13 @@ export default class Spreadsheet extends Component {
             <tr id={'r0l'} key={'r0l'}>
               <th className="border border-left-top" id={'c0r0_'} key={'c0r0_'}>{'/'}</th>
               {range(this.columns).map(col_num =>
-                  <BorderCell key={'c' + col_num + '_'} id={'c' + col_num + '_'} className="border-top" content={Alphabet[col_num]} />
+                  <BorderCell key={'c' + col_num + '_'} id={'c' + col_num + '_'} className="border-top" content={Alphabet[col_num] /* TODO: does this work for i.e. ABC (something with more than one letter) */} />
               )}
             </tr>
             {range(this.rows).map(row_num =>
               <tr id={'r' + row_num} key={'r' + row_num}>
                 <BorderCell key={'r' + row_num + '_'} id={'r' + row_num + '_'} className="border-left" content={row_num+1} />
-                {range(this.columns).map(y => this.render_cell(this.data[row_num][y]))}
+                {range(this.columns).map(col_num => this.render_cell(this.data[row_num][col_num]))}
               </tr>
             )}
           </tbody>
